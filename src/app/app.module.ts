8bddd195c75da7d4ee5743f602be39870ce03510
Kapil Kumar <k.kumar@wimionline.be>
2018-06-21 14:57:19 +0530

    restructing of directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { TestsComponent } from './tests/tests.component';
-import { DoctorsComponent } from './doctors/doctors.component';
-import { AppointmentsComponent } from './appointments/appointments.component';
-import { NewsComponent } from './news/news.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { FollowusComponent } from './followus/followus.component';
-import { MyaccountComponent } from './myaccount/myaccount.component';
+import { complist} from './complist';
+import { routelist } from './routelist';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'tests', component: TestsComponent},
-  { path: 'doctors', component: DoctorsComponent},
-  { path: 'appointments', component: AppointmentsComponent},
-  { path: 'news', component: NewsComponent},
-  { path: 'contact', component: ContactusComponent},
-  { path: 'follow', component: FollowusComponent},
-  { path: 'account', component: MyaccountComponent}
-];
+const appRoutes: Routes = routelist;
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    TestsComponent,
-    DoctorsComponent,
-    AppointmentsComponent,
-    NewsComponent,
-    ContactusComponent,
-    FollowusComponent,
-    MyaccountComponent
+    complist
   ],
   imports: [
     BrowserModule,
